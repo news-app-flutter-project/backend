6dbdaa80f9558487471608b4e852c0bc8f519aec
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-04-01 22:50:49 +0900

    [feat] - add NewsModelGenerator

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import { SEQUELIZE_CONFIGS } from "@/common/constants";
+import { NewsModel, NewsModelGenerator } from "@/database/models/news.models";
 
 const sequelize = new Sequelize(SEQUELIZE_CONFIGS);
 
@@ -13,12 +14,14 @@ declare global {
   interface DB {
     Sequelize: typeof Sequelize;
     sequelize: Sequelize;
+    News: typeof NewsModel;
   }
 }
 
 const db: DB = {
   Sequelize,
   sequelize,
+  News: NewsModelGenerator(sequelize),
 };
 
 export default db;
