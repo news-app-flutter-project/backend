038743ddc199c922fd04e347662217f92cef9d85
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-04-05 01:57:01 +0900

    [feat] added cron (not yet fully done)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import "module-alias/register";
 import useNewsApi from "@/apis/news_api";
 import paramsArr from "./apis/news_api_params";
+import { scheduleNewsUpdate } from "@/cron/news.jobs";
 import App from "./app";
 
 const app = new App({ port: Number(process.env.PORT), apiFn: useNewsApi });
@@ -9,5 +10,8 @@ const app = new App({ port: Number(process.env.PORT), apiFn: useNewsApi });
 // paramsArr.forEach(async (params) => {
 //   console.log(await app.test_api(params));
 // });
+
+// cron job
+scheduleNewsUpdate();
 
 app.bootstrap();
