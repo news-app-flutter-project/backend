b4a348cbb6e5fdec14fab43de103a56bba54483d
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-04-14 08:42:07 +0900

    [feat] validation middleware for payload

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 import Joi from "joi";
 import BadRequest from "./bad-request";
 
-const validationMiddleware = (schema: Joi.Schema): RequestHandler => {
+export const validationMiddleware = (schema: Joi.Schema): RequestHandler => {
   return async (
     req: Request,
     res: Response,
@@ -32,4 +32,35 @@ const validationMiddleware = (schema: Joi.Schema): RequestHandler => {
   };
 };
 
-export default validationMiddleware;
+export const validationPayload = (schema: Joi.Schema): RequestHandler => {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const validationOptions = {
+      abortEarly: false,
+      allowUnknown: true,
+      stripUnknown: true,
+    };
+
+    try {
+      const value = await schema.validateAsync(
+        JSON.parse(req.body.payload),
+        validationOptions
+      );
+      req.body = { ...req.body, value };
+      next();
+    } catch (e: any) {
+      // check e.name (joi library => validationError, validationErrorItem => can see more details on error ex) path, message, type
+      const errors: string[] = [];
+      e.details.forEach((error: Joi.ValidationErrorItem) => {
+        errors.push(error.message);
+      });
+      const badRequest = new BadRequest(errors.join(", "), errors);
+      res.status(badRequest.statusCode).send({
+        message: badRequest.errors,
+      });
+    }
+  };
+};
