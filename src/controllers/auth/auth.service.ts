5ca5a8e95f1e7fda869f19d222129415ef058892
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-04-14 09:38:02 +0900

    [fix] category type fix

--- src/controllers/auth/auth.service.ts
+++ src/controllers/auth/auth.service.ts
@@ -1,23 +1,40 @@
 import { authRepository } from "@/database/repositories/auth.repository";
 import kakaoLogin from "@/apis/kakao/kakaoLogin";
 import kakaoId from "@/apis/kakao/kakaoId";
+import uploadImageCloud from "@/apis/cloudinary/cloudinary";
 import { registerParams } from "@/utils/kakaoRegisterParams";
 
 declare global {
-  interface UserRegisterData {
+  interface KakaoRegisterData {
     code: string;
+  }
+  interface AppRegisterData {
+    profile_img: string;
+    name: string;
+    birthday: string;
+    sex: Sex;
+    category:
+      | [Category]
+      | [Category, Category]
+      | [Category, Category, Category];
+    age: Age;
+    auth_id?: number;
   }
 }
 
 export const authService = {
   repository: authRepository,
 
-  async registerUser(userRegisterData: UserRegisterData) {
+  async registerKakao(userRegisterData: KakaoRegisterData) {
     const { code } = userRegisterData;
     const kakao_login_response = await kakaoLogin(code);
     const kakao_info = await kakaoId(kakao_login_response.access_token);
     const kakaoData = registerParams(kakao_info, kakao_login_response);
-    const newUser = await this.repository.registerUser(kakaoData);
+    const newUser = await this.repository.registerKakao(kakaoData);
     return newUser;
+  },
+
+  async registerApp(data: AppRegisterData) {
+    console.log(await uploadImageCloud("asdf"));
   },
 };
