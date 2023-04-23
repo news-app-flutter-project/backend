1d93efcca509079577f8cc4da330a93470811eee
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-04-23 15:36:57 +0900

    [feat] lifestyleConvert utils

---
+++ src/utils/lifeStyleConvert.ts
@@ -0,0 +1,22 @@
+declare global {
+  type ConvertedCategory =
+    | Exclude<Category, "lifestyle">
+    | "environment"
+    | "food"
+    | "top"
+    | "tourism"
+    | "health";
+}
+
+export const lifeStyleConvert = (
+  categories: Category[]
+): ConvertedCategory[] => {
+  const newCategories = categories.filter((c) => c !== "lifestyle");
+  const convertedCategories: ConvertedCategory[] = [];
+
+  if (categories.includes("lifestyle")) {
+    convertedCategories.push("environment", "food", "top", "tourism", "health");
+  }
+
+  return [...newCategories, ...convertedCategories] as ConvertedCategory[];
+};
