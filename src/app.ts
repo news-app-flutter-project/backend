9009276887c102e3d35af7fa1fcef0112bfda02a
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-05-14 15:18:52 +0900

    [fix] swagger fixed thank 원영님

--- src/app.ts
+++ src/app.ts
@@ -44,7 +44,7 @@ class App {
     private initializeMiddleware() {
         this.express.use(helmet());
         this.express.use(cors());
-        // this.express.use(xss());
+        this.express.use(xss());
         this.express.use(morgan('dev'));
         this.express.use(express.json());
         this.express.use(express.urlencoded({ extended: false }));
